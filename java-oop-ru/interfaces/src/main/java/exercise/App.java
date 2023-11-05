d188f0f0b15ebf6ed25444be0db346ba9ebeefd5
@hexlet/cli <support@hexlet.io>
2023-11-05 14:20:31 +0200

    submit java-oop-ru/interfaces

---
+++ java-oop-ru/interfaces/src/main/java/exercise/App.java
@@ -0,0 +1,14 @@
+package exercise;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+// BEGIN
+public static List<String> buildApartmentsList(List<Home> homes, int index) {
+        return homes.stream()
+        .sorted((home1, home2) -> Double.compare(home1.getArea(), home2.getArea()))
+        .limit(index)
+        .map(home -> home.toString())
+        .collect(Collectors.toList());
+        }
+// END
---
+++ java-oop-ru/interfaces/src/main/java/exercise/Home.java
@@ -0,0 +1,8 @@
+package exercise;
+
+// BEGIN
+public interface Home {
+    int getArea();
+    int compareTo(Home another);
+}
+// END
